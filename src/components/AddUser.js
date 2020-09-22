7a167f0d384a9668b621ed6375b05a91da593d9c
max <mrom06@ya.ru>
2020-09-22 09:10:00 +0300

    Fixed user add error

--- src/components/AddUser.js
+++ src/components/AddUser.js
@@ -1,5 +1,6 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import Button from '@material-ui/core/Button'
+import { usersRef } from '../firebase'
 
 export default function AddUser() {
   return (
