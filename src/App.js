b257b76a4e31198344797b45970565b675d17fa5
max <mrom06@ya.ru>
2020-09-22 19:02:45 +0300

    Moved App to separated file

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { useState, Suspense } from 'react'
+
+import Login from './components/Login'
+import UserTable from './components/UserTable'
+import AddUser from './components/AddUser'
+import GlobalLoading from './components/GlobaLoading'
+
+export default function App() {
+  let [loggedIn, setLoggedIn] = useState(false)
+
+  return (
+    <Suspense fallback={<GlobalLoading />}>
+      <Login loggedIn={loggedIn} setLoggedIn={setLoggedIn} />
+      {loggedIn && <AddUser />}
+      <UserTable loggedIn={loggedIn} />
+    </Suspense>
+  )
+}
--- src/index.js
+++ src/index.js
@@ -1,21 +1,6 @@
-import React, { useState, Suspense } from 'react';
+import React from 'react';
 import { render } from 'react-dom';
-import Login from './components/Login'
-import UserTable from './components/UserTable';
-import AddUser from './components/AddUser'
-import GlobalLoading from './components/GlobaLoading'
-
-function App() {
-  let [loggedIn, setLoggedIn] = useState(false);
-
-  return (
-    <Suspense fallback={<GlobalLoading />}>
-      <Login loggedIn={loggedIn} setLoggedIn={setLoggedIn} />
-      {loggedIn && <AddUser/>}
-      <UserTable loggedIn={loggedIn} />
-    </Suspense>
-  )
-}
+import App from './App.js'
 
 let el = document.createElement('div');
 render(<App />, el);
